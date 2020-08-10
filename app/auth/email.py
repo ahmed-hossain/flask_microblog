36e15c64e83e069e212d6c167686f16891294735
sami <ahmedhossain.buet@gmail.com>
2020-08-11 01:10:14 +0600

    fixed password reset

--- app/auth/email.py
+++ app/auth/email.py
@@ -6,7 +6,7 @@ def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(
         '[Microblog] reset your password',
-        sender=app.config['ADMINS'][0],
+        sender=current_app.config['ADMINS'][0],
         recipients=[user.email],
         text_body=render_template('email/password_reset.txt', user=user, token=token),
         html_body=render_template('email/password_reset.html', user=user, token=token)
