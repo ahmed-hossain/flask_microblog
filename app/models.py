e0728cc5f7b032f2e82244ef6178af60d0320b81
sami <ahmedhossain.buet@gmail.com>
2020-08-14 02:57:45 +0600

    automatic message notification update using ajax

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
+import json
 
 from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -112,6 +113,8 @@ class User(UserMixin, db.Model):
         'Message', foreign_keys='Message.recipient_id',
         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship(
+        'Notification', backref='user', lazy='dynamic')
 
     def avatar(self,size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
@@ -162,6 +165,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -191,3 +200,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(140), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(self.payload_json)
--- microblog.py
+++ microblog.py
@@ -1,11 +1,14 @@
 from app import create_app, db
-from app.models import User, Post
+from app.models import User, Post, Message, Notification
+
 
 app = create_app()
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post,
+            'Message': Message, 'Notification': Notification}
 
 # TODO: Add user search
 # TODO: Make the form more user-friendly by adding error message
